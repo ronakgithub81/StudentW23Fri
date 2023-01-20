dbfb98015133f8f72757e1641f07d6e54010808a
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-01-20 13:42:40 -0500

    First Commit

---
+++ src/studentw23fri/StudentW23Fri.java
@@ -0,0 +1,41 @@
+
+
+package studentw23fri;
+
+/**
+ *
+ * @author Ronak
+ */
+public class StudentW23Fri {
+    
+    private String studentId;
+    private String studentName;
+
+    public StudentW23Fri(String studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    
+
+    
+    
+    
+    
+
+}//end of class
