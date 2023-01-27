947ff4c9cf8537e61a002581a1a3e6e9622d02fe
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-01-27 12:24:40 -0500

    added new private variable address and getters/setters

--- src/studentw23fri/StudentW23Fri.java
+++ src/studentw23fri/StudentW23Fri.java
@@ -10,10 +10,19 @@ public class StudentW23Fri {
     
     private String studentId;
     private String studentName;
+    private String address;
 
     public StudentW23Fri(String studentId, String studentName) {
         this.studentId = studentId;
         this.studentName = studentName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getStudentId() {
